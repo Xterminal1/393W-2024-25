7a6adc5756be64163fdc264407233d0939adf3af
Xterminal1 <sathviklolla@gmail.com>
2024-10-15 16:56:38 -0700

    organized imports

--- include/vex.h
+++ include/vex.h
@@ -12,6 +12,9 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
+
+// extra imports
 #include <string>
 #include <algorithm>
 #include <array>
@@ -19,7 +22,6 @@
 #include <sstream>
 #include <vector>
 #include <typeinfo>
-//#include <string.h>
 #include <iomanip>
 
 #include "v5.h"
